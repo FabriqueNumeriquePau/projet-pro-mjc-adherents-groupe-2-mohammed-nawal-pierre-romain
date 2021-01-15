c31af8f8f7cd08aac8644fb4b50eef5699acd9e2
Nawal003 <nawal.zouihig@gmail.com>
2021-01-15 13:40:56 +0100

    adhérents

--- adhérents/dist/adhérents.dev.js
+++ adhérents/dist/adhérents.dev.js
@@ -1,12 +1,1 @@
-// document.getElementsByClassName('entete').onscroll = function () { myFunction() };
-// let header = document.getElementById("myHeader");
-// let sticky = header.offsetTop;
-// function myFunction() {
-//     if (document.getElementsByClassName('entete').pageXOffset > sticky) {
-//         header.classList.add("sticky");
-//     } else {
-//         header.classList.remove("sticky");
-//     }
-// };
-// console.log(header);
 "use strict";
