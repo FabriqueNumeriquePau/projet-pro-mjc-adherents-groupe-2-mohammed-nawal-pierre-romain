94d6944f056ba368a04c14d26f96c208e16a8f03
Nawal003 <nawal.zouihig@gmail.com>
2021-01-18 16:59:24 +0100

    18/01

--- tableaux/dist/adhérents.dev.js
+++ tableaux/dist/adhérents.dev.js
@@ -3,7 +3,12 @@
 var modal = document.getElementById('wrapper-modal');
 console.log(modal);
 var ajouter = document.getElementById('ajouter');
+var annuler = document.getElementById('cancel');
 ajouter.addEventListener('click', function () {
   modal.style.visibility = 'visible';
   modal.style.opacity = '1';
+});
+annuler.addEventListener('click', function () {
+  modal.style.visibility = 'hidden';
+  modal.style.opacity = 'O';
 });
--- tableaux/tableaux.js
+++ tableaux/tableaux.js
@@ -1,8 +1,13 @@
 let modal = document.getElementById('wrapper-modal');
 console.log(modal);
 let ajouter = document.getElementById('ajouter');
+let annuler = document.getElementById('cancel');
 
 ajouter.addEventListener('click', () =>{
 modal.style.visibility = 'visible';
 modal.style.opacity = '1';
+});
+annuler.addEventListener('click', () => {
+    modal.style.visibility = 'hidden';
+    modal.style.opacity = 'O';
 });
